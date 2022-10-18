b793d112c3a1bd8fb94e96b59380e3a2f864aaf7
Hazim J <hazim.jumali@gmail.com>
2022-10-18 18:13:48 +1100

    Add the basic UserOperation sanity checks (#6)

--- pkg/client/instance.go
+++ pkg/client/instance.go
@@ -1,7 +1,11 @@
 package client
 
 import (
+	"errors"
+
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stackup-wallet/stackup-bundler/pkg/entrypoint"
 	"github.com/stackup-wallet/stackup-bundler/pkg/userop"
 )
 
@@ -10,15 +14,49 @@ type Instance struct {
 	supportedEntryPoints []string
 }
 
+func (i *Instance) parseEntryPointAddress(ep string) (common.Address, error) {
+	for _, v := range i.supportedEntryPoints {
+		addr := common.HexToAddress(v)
+		if addr == common.HexToAddress(ep) {
+			return addr, nil
+		}
+	}
+
+	return common.Address{}, errors.New("entryPoint: Implementation not supported")
+}
+
 // Implements the method call for eth_sendUserOperation.
 // Returns true if userOp was accepted otherwise returns an error.
-func (i *Instance) Eth_sendUserOperation(op map[string]interface{}) (bool, error) {
-	userop, err := userop.New(op)
+func (i *Instance) Eth_sendUserOperation(op map[string]interface{}, ep string) (bool, error) {
+	epAddr, err := i.parseEntryPointAddress(ep)
+	if err != nil {
+		return false, err
+	}
+	entryPoint, err := entrypoint.NewEntrypoint(epAddr, i.ethClient)
 	if err != nil {
 		return false, err
 	}
 
+	userop, err := userop.New(op)
+	if err != nil {
+		return false, err
+	}
 	if err := userop.CheckSender(i.ethClient); err != nil {
+		return false, err
+	}
+	if err := userop.CheckVerificationGasLimits(i.ethClient); err != nil {
+		return false, err
+	}
+	if err := userop.CheckPaymasterAndData(i.ethClient, entryPoint); err != nil {
+		return false, err
+	}
+	if err := userop.CheckCallGasLimit(i.ethClient); err != nil {
+		return false, err
+	}
+	if err := userop.CheckFeePerGas(i.ethClient); err != nil {
+		return false, err
+	}
+	if err := userop.CheckDuplicate(i.ethClient); err != nil {
 		return false, err
 	}
 
