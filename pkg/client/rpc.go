fdeb870d7a986e4965d6d6f86c4717e34cc83136
Hazim J <hazim.jumali@gmail.com>
2022-11-19 16:12:27 +1100

    Clean: code layout

--- pkg/client/rpc.go
+++ pkg/client/rpc.go
@@ -5,6 +5,11 @@ type RpcAdapter struct {
 	client *Client
 }
 
+// NewRpcAdapter initializes a new RpcAdapter which can be used with a JSON-RPC server.
+func NewRpcAdapter(client *Client) *RpcAdapter {
+	return &RpcAdapter{client}
+}
+
 // Eth_sendUserOperation routes eth_sendUserOperation method calls to client.SendUserOperation.
 func (r *RpcAdapter) Eth_sendUserOperation(op map[string]any, ep string) (bool, error) {
 	return r.client.SendUserOperation(op, ep)
@@ -13,9 +18,4 @@ func (r *RpcAdapter) Eth_sendUserOperation(op map[string]any, ep string) (bool, 
 // Eth_supportedEntryPoints routes eth_supportedEntryPoints method calls to client.supportedEntryPoints.
 func (r *RpcAdapter) Eth_supportedEntryPoints() ([]string, error) {
 	return r.client.SupportedEntryPoints()
-}
-
-// NewRpcAdapter initializes a new RpcAdapter which can be used with a JSON-RPC server.
-func NewRpcAdapter(client *Client) *RpcAdapter {
-	return &RpcAdapter{client}
 }
