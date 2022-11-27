0677fa3065e099d2d4ab73a40f20a4b83c3ef750
Hazim J <hazim.jumali@gmail.com>
2022-11-27 18:13:58 +1100

    Add Caller() to logger (#39)

--- internal/logger/zerolog.go
+++ internal/logger/zerolog.go
@@ -10,6 +10,6 @@ import (
 
 // NewZeroLogr returns a Zerolog logger wrapped in a go-logr/logr interface.
 func NewZeroLogr() logr.Logger {
-	zl := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	zl := zerolog.New(os.Stderr).With().Caller().Timestamp().Logger()
 	return zerologr.New(&zl)
 }
