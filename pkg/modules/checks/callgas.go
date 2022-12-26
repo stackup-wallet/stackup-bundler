54347cfee3f5a626fc85cd41b7725d7d50c6079a
Hazim J <hazim.jumali@gmail.com>
2022-12-26 15:59:57 +1100

    clean

--- pkg/modules/checks/callgas.go
+++ pkg/modules/checks/callgas.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stackup-wallet/stackup-bundler/pkg/userop"
 )
 
+// ValidateCallGasLimit checks the callGasLimit is at least the cost of a CALL with non-zero value.
 func ValidateCallGasLimit(op *userop.UserOperation) error {
 	cg := gas.NewDefaultOverhead().NonZeroValueCall()
 	if op.CallGasLimit.Cmp(cg) < 0 {
