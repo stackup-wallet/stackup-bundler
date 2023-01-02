eb7bc89c3cf74e460f01686f5e00985f73dc42ae
Hazim J <hazim.jumali@gmail.com>
2023-01-02 12:24:10 +1100

    clean

--- pkg/gas/types.go
+++ pkg/gas/types.go
@@ -2,6 +2,7 @@ package gas
 
 import "math/big"
 
+// GasEstimates provides estimate values for all gas fields in a UserOperation.
 type GasEstimates struct {
 	PreVerificationGas *big.Int `json:"preVerificationGas"`
 	VerificationGas    *big.Int `json:"verificationGas"`
