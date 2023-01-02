a93b86bd06396d13d2df49df89d753b4050ac9d1
Hazim J <hazim.jumali@gmail.com>
2023-01-02 12:10:41 +1100

    fix comments

--- pkg/client/utils.go
+++ pkg/client/utils.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stackup-wallet/stackup-bundler/pkg/entrypoint"
 )
 
+// GetUserOpReceiptFunc is a general interface for fetching a UserOperationReceipt given a userOpHash and
+// EntryPoint address.
 type GetUserOpReceiptFunc = func(hash string, ep common.Address) (*entrypoint.UserOperationReceipt, error)
 
 func getUserOpReceiptNoop() GetUserOpReceiptFunc {
@@ -17,6 +19,8 @@ func getUserOpReceiptNoop() GetUserOpReceiptFunc {
 	}
 }
 
+// GetUserOpReceiptWithEthClient returns an implementation of GetUserOpReceiptFunc that relies on an eth
+// client to fetch a UserOperationReceipt.
 func GetUserOpReceiptWithEthClient(eth *ethclient.Client) GetUserOpReceiptFunc {
 	return func(hash string, ep common.Address) (*entrypoint.UserOperationReceipt, error) {
 		return entrypoint.GetUserOperationReceipt(eth, hash, ep)
