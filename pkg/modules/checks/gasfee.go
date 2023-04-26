b14d8bba5ae81a8d3ba139f3e636bba01f4b9197
hazim <hazim.jumali@gmail.com>
2023-04-26 11:51:40 +1000

    Add support for legacy fee mode (#153)

--- pkg/modules/checks/gasfee.go
+++ pkg/modules/checks/gasfee.go
@@ -7,11 +7,20 @@ import (
 )
 
 // ValidateFeePerGas checks the maxFeePerGas is sufficiently high to be included with the current
-// block.basefee.
+// block.basefee. Alternatively, if basefee is not supported, then check that maxPriorityFeePerGas is equal to
+// maxFeePerGas as a fallback.
 func ValidateFeePerGas(op *userop.UserOperation, gbf GetBaseFeeFunc) error {
 	bf, err := gbf()
 	if err != nil {
 		return err
+	}
+
+	if bf == nil {
+		if op.MaxPriorityFeePerGas.Cmp(op.MaxFeePerGas) != 0 {
+			return fmt.Errorf("legacy fee mode: maxPriorityFeePerGas must equal maxFeePerGas")
+		}
+
+		return nil
 	}
 
 	if op.MaxFeePerGas.Cmp(bf) < 0 {
