3e70b53306f0ca3c1acf2d322707e43438b10408
hazim <hazim.jumali@gmail.com>
2023-07-17 15:36:17 +1000

    Fix typo in eth_estimateUserOperationGas (#206)

--- pkg/gas/types.go
+++ pkg/gas/types.go
@@ -4,7 +4,10 @@ import "math/big"
 
 // GasEstimates provides estimate values for all gas fields in a UserOperation.
 type GasEstimates struct {
-	PreVerificationGas *big.Int `json:"preVerificationGas"`
-	VerificationGas    *big.Int `json:"verificationGas"`
-	CallGasLimit       *big.Int `json:"callGasLimit"`
+	PreVerificationGas   *big.Int `json:"preVerificationGas"`
+	VerificationGasLimit *big.Int `json:"verificationGasLimit"`
+	CallGasLimit         *big.Int `json:"callGasLimit"`
+
+	// TODO: Deprecate in v0.7
+	VerificationGas *big.Int `json:"verificationGas"`
 }
