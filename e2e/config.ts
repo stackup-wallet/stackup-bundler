fe06f84b45b8d9611c44381f0bbbf42e7bf52a82
hazim <hazim.jumali@gmail.com>
2023-07-26 12:46:56 +1000

    Update TestGas contract to send value in recursive calls (#224)

--- e2e/config.ts
+++ e2e/config.ts
@@ -14,7 +14,7 @@ const config: IConfig = {
   bundlerUrl: "http://localhost:4337",
   testERC20Token: "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B",
   // https://github.com/stackup-wallet/contracts/blob/main/contracts/test/TestGas.sol
-  testGas: "0x310de3671FCDA16eC24433e7610c2F48DEE7f8c2",
+  testGas: "0xd98206114295d553625cFF946D8aEc3aa790db3A",
 };
 
 export default config;
