77da1eb3c190f437de977fd0fdcb084cc2367bc1
hazim <hazim.jumali@gmail.com>
2023-08-03 10:58:53 +1000

    Allow access to associated storage for new accounts deployed with a staked factory (#202)

--- internal/config/values.go
+++ internal/config/values.go
@@ -76,7 +76,7 @@ func GetValues() *Values {
 	viper.SetDefault("erc4337_bundler_port", 4337)
 	viper.SetDefault("erc4337_bundler_data_directory", "/tmp/stackup_bundler")
 	viper.SetDefault("erc4337_bundler_supported_entry_points", "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789")
-	viper.SetDefault("erc4337_bundler_max_verification_gas", 1500000)
+	viper.SetDefault("erc4337_bundler_max_verification_gas", 3000000)
 	viper.SetDefault("erc4337_bundler_max_batch_gas_limit", 25000000)
 	viper.SetDefault("erc4337_bundler_max_ops_for_unstaked_sender", 4)
 	viper.SetDefault("erc4337_bundler_blocks_in_the_future", 25)
