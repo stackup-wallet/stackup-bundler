d8663278e141944432177c71d98af139fdcf57f0
hazim-j <hazim.jumali@gmail.com>
2023-08-10 16:22:38 +0800

    Only account for execution gas in MaintainGasLimit

--- pkg/modules/batch/gaslimit.go
+++ pkg/modules/batch/gaslimit.go
@@ -3,6 +3,7 @@ package batch
 import (
 	"math/big"
 
+	"github.com/stackup-wallet/stackup-bundler/pkg/gas"
 	"github.com/stackup-wallet/stackup-bundler/pkg/modules"
 	"github.com/stackup-wallet/stackup-bundler/pkg/userop"
 )
@@ -10,11 +11,21 @@ import (
 // MaintainGasLimit returns a BatchHandlerFunc that ensures the max gas used from the entire batch does not
 // exceed the allowed threshold.
 func MaintainGasLimit(maxBatchGasLimit *big.Int) modules.BatchHandlerFunc {
+	// See comment in pkg/modules/checks/gas.go
+	staticOv := gas.NewDefaultOverhead()
+
 	return func(ctx *modules.BatchHandlerCtx) error {
 		bat := []*userop.UserOperation{}
 		sum := big.NewInt(0)
 		for _, op := range ctx.Batch {
-			sum = big.NewInt(0).Add(sum, op.GetMaxGasAvailable())
+			static, err := staticOv.CalcPreVerificationGas(op)
+			if err != nil {
+				return err
+			}
+			mgl := big.NewInt(0).Sub(op.GetMaxGasAvailable(), op.PreVerificationGas)
+			mga := big.NewInt(0).Add(mgl, static)
+
+			sum = big.NewInt(0).Add(sum, mga)
 			if sum.Cmp(maxBatchGasLimit) >= 0 {
 				break
 			}
