24a5fc6cf6a69b745f9369f3221cec3ef637f728
hazim-j <hazim.jumali@gmail.com>
2023-12-04 12:17:28 +1100

    better err

--- pkg/state/values.go
+++ pkg/state/values.go
@@ -3,6 +3,6 @@ package state
 import "errors"
 
 var (
-	ErrBadKey   = errors.New("cannot decode override address")
+	ErrBadKey   = errors.New("cannot decode key to address")
 	ErrBadValue = errors.New("cannot decode override account")
 )
