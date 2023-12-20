414aeb38ad5b736e871b00e00f44ed448f487bb0
NickSolante <nicksolante1@gmail.com>
2023-12-20 21:22:19 +1100

    improve on PR comments

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -7,23 +7,23 @@ import (
 )
 
 var (
-	EthereumChainID         = big.NewInt(1)
-	GoerliChainID           = big.NewInt(5)
-	SepoliaChainID          = big.NewInt(11155111)
-	ArbitrumOneChainID      = big.NewInt(42161)
-	ArbitrumGoerliChainID   = big.NewInt(421613)
-	ArbitrumSepoliaChainID  = big.NewInt(421614)
-	OptimismChainID         = big.NewInt(10)
-	OptimismGoerliChainID   = big.NewInt(420)
-	OptimismSepoliaChainID  = big.NewInt(11155420)
-	BaseChainID             = big.NewInt(8453)
-	BaseGoerliChainID       = big.NewInt(84531)
-	BaseSepoliaChainID      = big.NewInt(84532)
-	LyraChainID             = big.NewInt(957)
-	LyraSepoliaChainID      = big.NewInt(902)
-	Ancient8CSepoliaChainID = big.NewInt(2863311531)
+	EthereumChainID        = big.NewInt(1)
+	GoerliChainID          = big.NewInt(5)
+	SepoliaChainID         = big.NewInt(11155111)
+	ArbitrumOneChainID     = big.NewInt(42161)
+	ArbitrumGoerliChainID  = big.NewInt(421613)
+	ArbitrumSepoliaChainID = big.NewInt(421614)
+	OptimismChainID        = big.NewInt(10)
+	OptimismGoerliChainID  = big.NewInt(420)
+	OptimismSepoliaChainID = big.NewInt(11155420)
+	BaseChainID            = big.NewInt(8453)
+	BaseGoerliChainID      = big.NewInt(84531)
+	BaseSepoliaChainID     = big.NewInt(84532)
+	LyraChainID            = big.NewInt(957)
+	LyraSepoliaChainID     = big.NewInt(902)
+	Ancient8SepoliaChainID = big.NewInt(2863311531)
 
-	OpStack = mapset.NewSet[*big.Int](
+	OpStackChains = mapset.NewSet[*big.Int](
 		OptimismChainID,
 		OptimismGoerliChainID,
 		OptimismSepoliaChainID,
@@ -32,12 +32,6 @@ var (
 		BaseSepoliaChainID,
 		LyraChainID,
 		LyraSepoliaChainID,
-		Ancient8CSepoliaChainID,
-	)
-
-	ArbStack = mapset.NewSet[*big.Int](
-		ArbitrumOneChainID,
-		ArbitrumGoerliChainID,
-		ArbitrumSepoliaChainID,
+		Ancient8SepoliaChainID,
 	)
 )
--- internal/start/private.go
+++ internal/start/private.go
@@ -83,12 +83,14 @@ func PrivateMode() {
 	}
 
 	ov := gas.NewDefaultOverhead()
-	if ok := config.ArbStack.Contains(chain); ok {
+	if chain.Cmp(config.ArbitrumOneChainID) == 0 ||
+		chain.Cmp(config.ArbitrumGoerliChainID) == 0 ||
+		chain.Cmp(config.ArbitrumSepoliaChainID) == 0 {
 		ov.SetCalcPreVerificationGasFunc(gas.CalcArbitrumPVGWithEthClient(rpc, conf.SupportedEntryPoints[0]))
 		ov.SetPreVerificationGasBufferFactor(16)
 	}
 
-	if ok := config.OpStack.Contains(chain); ok {
+	if ok := config.OpStackChains.Contains(chain); ok {
 		ov.SetCalcPreVerificationGasFunc(
 			gas.CalcOptimismPVGWithEthClient(rpc, chain, conf.SupportedEntryPoints[0]),
 		)
