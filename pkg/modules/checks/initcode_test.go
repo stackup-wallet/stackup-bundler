2bbdcd2ef47321e58987f667297e118a0dc87c2e
hazim-j <hazim.jumali@gmail.com>
2024-01-08 12:38:02 +1100

    initcode test

--- pkg/modules/checks/initcode_test.go
+++ pkg/modules/checks/initcode_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/stackup-wallet/stackup-bundler/internal/testutils"
 )
 
-// TestInitCodeDNE calls checks.ValidateInitCode where initCode does not exist. Expect nil.
 func TestInitCodeDNE(t *testing.T) {
 	op := testutils.MockValidInitUserOp()
 	op.InitCode = []byte{}
@@ -17,14 +16,21 @@ func TestInitCodeDNE(t *testing.T) {
 	}
 }
 
-// TestInitCodeContainsAddress calls checks.ValidateInitCode where initCode exist without a valid address.
-// Expect error.
-func TestInitCodeContainsAddress(t *testing.T) {
+func TestInitCodeContainsBadAddress(t *testing.T) {
 	op := testutils.MockValidInitUserOp()
 	op.InitCode = []byte("1234")
 	err := ValidateInitCode(op)
 
 	if err == nil {
 		t.Fatalf("got nil, want err")
+	}
+}
+
+func TestInitCodeExists(t *testing.T) {
+	op := testutils.MockValidInitUserOp()
+	err := ValidateInitCode(op)
+
+	if err != nil {
+		t.Fatalf(`got err %v, want nil`, err)
 	}
 }
